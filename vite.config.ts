1746a9de907f86d878673f077d0e60474ff44313
Fernanda Oshiro <fernanda.sayuri@outlook.com>
2025-04-14 22:44:16 -0300

    chore: configure Vitest to support TypeScript path aliases

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vite'
+import path from 'path'
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'node',
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+})
