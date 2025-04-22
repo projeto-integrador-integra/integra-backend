7126c3ca718e4a2b02da82710f55eef3982ac463
Fernanda Oshiro <fernanda.sayuri@outlook.com>
2025-04-22 20:19:09 -0300

    fix: update getCookieOptions to enforce secure cookies and adjust sameSite policy

--- src/config/cookie.ts
+++ src/config/cookie.ts
@@ -1,13 +1,14 @@
 import type { CookieOptions, Request } from 'express'
 
 export function getCookieOptions(req: Request): CookieOptions {
+  console.log('getCookieOptions', req.hostname, process.env.NODE_ENV)
   const isLocalhost = req.hostname === 'localhost' || req.hostname.startsWith('127.')
-  const isSecure = process.env.NODE_ENV === 'production' && !isLocalhost
 
   return {
     httpOnly: true,
-    secure: isSecure,
-    sameSite: isSecure ? 'strict' : 'lax',
+    secure: true,
+    sameSite: 'none',
+    ...(isLocalhost ? {} : { domain: '.chambytes.dev' }),
     maxAge: 1000 * 60 * 60,
   }
 }
