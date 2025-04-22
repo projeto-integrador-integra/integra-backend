a9db968636b5df7f06dd38d4a5b00991e1fd3ee3
Fernanda Oshiro <fernanda.sayuri@outlook.com>
2025-04-22 20:24:13 -0300

    fix: update getCookieOptions to set secure cookies to false and adjust sameSite policy to lax

--- src/config/cookie.ts
+++ src/config/cookie.ts
@@ -2,13 +2,11 @@ import type { CookieOptions, Request } from 'express'
 
 export function getCookieOptions(req: Request): CookieOptions {
   console.log('getCookieOptions', req.hostname, process.env.NODE_ENV)
-  const isLocalhost = req.hostname === 'localhost' || req.hostname.startsWith('127.')
 
   return {
     httpOnly: true,
-    secure: true,
-    sameSite: 'none',
-    ...(isLocalhost ? {} : { domain: '.chambytes.dev' }),
+    secure: false,
+    sameSite: 'lax',
     maxAge: 1000 * 60 * 60,
   }
 }
