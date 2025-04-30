65ffe3a0128385d0b623ff10091c0d0986555712
Fernanda Oshiro <fernanda.sayuri@outlook.com>
2025-04-29 23:32:59 -0300

    refactor: standardize naming convention for projectRepository in ProjectService

--- src/services/project.service.ts
+++ src/services/project.service.ts
@@ -4,21 +4,21 @@ import { Project } from '@/models/domain/project'
 import { ProjectRepository } from '@/repositories/project.repository'
 
 export class ProjectService {
-  constructor(private readonly ProjectRepository: ProjectRepository) {}
+  constructor(private readonly projectRepository: ProjectRepository) {}
 
   async register(data: Project): Promise<Project> {
-    const existingProject = await this.ProjectRepository.findSimilarProject({
+    const existingProject = await this.projectRepository.findSimilarProject({
       userId: data.creatorId,
       title: data.name,
     })
     if (existingProject.length > 0)
       throw new AppError('Project already exists', 409, 'PROJECT_ALREADY_EXISTS')
 
-    const userProjects = await this.ProjectRepository.listProjects({ createdBy: data.creatorId })
+    const userProjects = await this.projectRepository.listProjects({ createdBy: data.creatorId })
     if (userProjects.total >= MAX_PROJECTS_PER_USER)
       throw new AppError('User already has 3 projects', 409, 'USER_PROJECT_LIMIT_REACHED')
 
-    const newProject = await this.ProjectRepository.create(data)
+    const newProject = await this.projectRepository.create(data)
     return newProject
   }
 }
